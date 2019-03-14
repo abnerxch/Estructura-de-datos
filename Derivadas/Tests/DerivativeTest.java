27e5f5ce1c6e1529252d3c063dfe38b77c33cf71
Abner <axocop@ufm.edu>
2019-03-14 11:17:30 -0600

    Starting tests
    Starting tests

---
+++ Derivadas/Tests/DerivativeTest.java
@@ -0,0 +1,18 @@
+import com.company.Main;
+import org.junit.Test;
+
+public class DerivativeTest {
+    @Test
+    public void ExpNodeTest(){
+        ExpNodeTest();
+    }
+
+    @Test
+    public void factorTreeTest(){
+        Main main = new Main();
+        char opTest = CustomV1.TextIO.getAnyChar();
+        ExpNodeTest();
+
+    }
+
+}
