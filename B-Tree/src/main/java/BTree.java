92f2cd087d7d3398add2ef97c9112193798702b4
Abner <axocop@ufm.edu>
2019-03-19 00:23:23 -0600

    Updating names
    Updating names

--- B-Tree/src/main/java/BTree.java
+++ B-Tree/src/main/java/BTree.java
@@ -2,29 +2,29 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class BTree <Key extends Comparable<Key>, Value>{
-    private static final int MaxChildren = 2;
+    private static final int MaxChildren = 4;
 
     private Node root;
     private int height;
-    private int n;
+    private int numberOfKeyValues;
 
     private static  class Node {
-        private int m;
+        private int numberOfChildren;
         private Entry[] children = new Entry[MaxChildren ];
 
-        private Node (int k ){
-            m = k;
+        private Node (int kChildren ){ //nodo con k Children
+            numberOfChildren = kChildren;
         }
     }
 
     private static class Entry{
-        private Comparable key;
-        private final Object val;
+        private Comparable keyEntry;
+        private final Object valueEntry;
         private BTree.Node next;
 
-        public Entry (Comparable key, Object val, BTree.Node next){
-            this.key = key;
-            this.val = val;
+        public Entry (Comparable keyEntry, Object valueEntry, BTree.Node next){
+            this.keyEntry = keyEntry;
+            this.valueEntry = valueEntry;
             this.next = next;
         }
     }
@@ -38,7 +38,7 @@ public class BTree <Key extends Comparable<Key>, Value>{
     }
 
     public int size(){
-        return n;
+        return numberOfKeyValues;
     }
 
     public int height(){
@@ -50,142 +50,142 @@ public class BTree <Key extends Comparable<Key>, Value>{
         return search(root, key, height);
     }
 
-    public Value search (Node x, Key key, int ht){
-        Entry[] children = x.children;
+    public Value search (Node xNumberOfNode, Key key, int heightOfNode){
+        Entry[] children = xNumberOfNode.children;
 
-        if(ht == 0){
-            for (int j = 0; j < x.m; j++) {
-                if(eq(key, children[j].key)) return (Value) children[j].val;
+        if(heightOfNode == 0){
+            for (int posicionJ = 0; posicionJ < xNumberOfNode.numberOfChildren; posicionJ++) {
+                if(equalsTo(key, children[posicionJ].keyEntry)) return (Value) children[posicionJ].valueEntry;
             }
         }
 
         else {
-            for (int j = 0; j < x.m; j++) {
-                if (j +1 == x.m || less(key, children[j+1].key))
-                    return search(children[j].next, key, ht-1);
+            for (int posicionJ = 0; posicionJ < xNumberOfNode.numberOfChildren; posicionJ++) {
+                if (posicionJ +1 == xNumberOfNode.numberOfChildren || lessThan(key, children[posicionJ+1].keyEntry))
+                    return search(children[posicionJ].next, key, heightOfNode-1);
             }
         }
         return null;
     }
 
-    public void put (Key key, Value val){
-        if(key == null) throw new IllegalArgumentException("Argumento a put() es NULL");
-        Node u = insert(root, key, val, height);
-        n++;
-        if(u==null) return;
+    public void put (Key keyPut, Value valuePut){
+        if(keyPut == null) throw new IllegalArgumentException("Argumento a put() es NULL");
+        Node putNode = insert(root, keyPut, valuePut, height);
+        numberOfKeyValues++;
+        if(putNode == null) return;
 
-        Node t = new Node(2);
-        t.children[0] = new Entry(root.children[0].key, null, root);
-        t.children[1] = new Entry(u.children[0].key, null, u);
-        root = t;
+        Node putNodeFather = new Node(2);
+        putNodeFather.children[0] = new Entry(root.children[0].keyEntry, null, root);
+        putNodeFather.children[1] = new Entry(putNode.children[0].keyEntry, null, putNode);
+        root = putNodeFather;
         height++;
     }
 
-    private Node insert(Node h, Key key, Value val, int ht){
-        int j;
-        Entry t = new Entry(key, val, null);
+    private Node insert(Node insertNode, Key insertKey, Value insertValue, int insertHeight){
+        int posicionI;
+        Entry insertNewNode = new Entry(insertKey, insertValue, null);
 
-        if( ht == 0){
-            for (j = 0; j < h.m; j++) {
-                if(less(key, h.children[j].key)) break;
+        if( insertHeight == 0){
+            for (posicionI = 0; posicionI < insertNode.numberOfChildren; posicionI++) {
+                if(lessThan(insertKey, insertNode.children[posicionI].keyEntry)) break;
             }
         }
 
         else {
-            for (j = 0; j < h.m; j++) {
-                if ((j + 1 == h.m) || less (key, h.children[j + 1].key)){
-                    Node u = insert(h.children[j++].next, key, val, ht-1);
-                    if(u == null) return null;
-                    t.key = u.children[0].key;
-                    t.next = u;
+            for (posicionI = 0; posicionI < insertNode.numberOfChildren; posicionI++) {
+                if ((posicionI + 1 == insertNode.numberOfChildren) || lessThan (insertKey, insertNode.children[posicionI + 1].keyEntry)){
+                    Node insertNewNodeFather = insert(insertNode.children[posicionI++].next, insertKey, insertValue, insertHeight-1);
+                    if(insertNewNodeFather == null) return null;
+                    insertNewNode.keyEntry = insertNewNodeFather.children[0].keyEntry;
+                    insertNewNode.next = insertNewNodeFather;
                     break;
                 }
             }
         }
 
-        for(int i = h.m; i > j; i--){
-            h.children[i] = h.children[i-1];
+        for(int posicionK = insertNode.numberOfChildren; posicionK > posicionI; posicionK--){
+            insertNode.children[posicionK] = insertNode.children[posicionK-1];
         }
-        h.children[j] = t;
-        h.m++;
-        if(h.m < MaxChildren  ) return null;
-        else return split(h);
+        insertNode.children[posicionI] = insertNewNode;
+        insertNode.numberOfChildren++;
+        if(insertNode.numberOfChildren < MaxChildren  ) return null;
+        else return split(insertNode);
     }
 
-    private Node split(Node h){
-        Node t = new Node(MaxChildren /2);
-        h.m = MaxChildren /2;
-        for (int j = 0; j < MaxChildren /2; j++) {
-            t.children[j] = h.children[MaxChildren /2+j];
+    private Node split(Node nodeToSplit){
+        Node newNodeToSplit = new Node(MaxChildren /2);
+        nodeToSplit.numberOfChildren = MaxChildren /2;
+        for (int posicionL = 0; posicionL < MaxChildren /2; posicionL++) {
+            newNodeToSplit.children[posicionL] = nodeToSplit.children[MaxChildren/2+posicionL];
         }
-        return t;
+        return newNodeToSplit;
     }
 
     public String toString(){
         return toString(root, height, "") + "\n";
     }
 
-    private String toString(Node h, int ht, String indent){
-        StringBuilder s = new StringBuilder();
-        Entry[] children = h.children;
+    private String toString(Node nodeToString, int heightToString, String indent){
+        StringBuilder newNodeToString = new StringBuilder();
+        Entry[] children = nodeToString.children;
 
-        if(ht == 0){
-            for (int j = 0; j < h.m; j++) {
-                s.append(indent + children[j].key + " " + children[j].val + "\n");
+        if(heightToString == 0){
+            for (int posicionM = 0; posicionM < nodeToString.numberOfChildren; posicionM++) {
+                newNodeToString.append(indent + children[posicionM].keyEntry + " " + children[posicionM].valueEntry + "\n");
             }
         }
 
         else{
-            for (int j =0; j < h.m; j++){
-                if (j > 0) s.append(indent + "(" + children[j].key + ")\n");
-                s.append(toString(children[j].next, ht-1, indent + "    "));
+            for (int posicionN =0; posicionN < nodeToString.numberOfChildren; posicionN++){
+                if (posicionN > 0) newNodeToString.append(indent + "(" + children[posicionN].keyEntry + ")\n");
+                newNodeToString.append(toString(children[posicionN].next, heightToString-1, indent + "    "));
             }
         }
-        return s.toString();
+        return newNodeToString.toString();
     }
 
-    private boolean less(Comparable k1, Comparable k2){
-        return k1.compareTo(k2) < 0;
+    private boolean lessThan(Comparable leaf1, Comparable leaf2){
+        return leaf1.compareTo(leaf2) < 0;
     }
 
-    private boolean eq(Comparable k1, Comparable k2){
-        return k1.compareTo(k2) == 0;
+    private boolean equalsTo(Comparable leaf1, Comparable leaf2){
+        return leaf1.compareTo(leaf2) == 0;
     }
 
     public static void main(String[ ] args) throws IOException  {
         String sCadena;
-        BTree<String, String> st = new BTree<String, String>();
+        BTree<String, String> stringNode = new BTree<String, String>();
 
-        st.put("www.cs.princeton.edu", "128.112.136.12");
-        st.put("www.cs.princeton.edu", "128.112.136.11");
-        st.put("www.princeton.edu",    "128.112.128.15");
-        st.put("www.yale.edu",         "130.132.143.21");
-        st.put("www.simpsons.com",     "209.052.165.60");
-        st.put("www.apple.com",        "17.112.152.32");
-        st.put("www.amazon.com",       "207.171.182.16");
-        st.put("www.ebay.com",         "66.135.192.87");
-        st.put("www.cnn.com",          "64.236.16.20");
-        st.put("www.google.com",       "216.239.41.99");
-        st.put("www.nytimes.com",      "199.239.136.200");
-        st.put("www.microsoft.com",    "207.126.99.140");
-        st.put("www.dell.com",         "143.166.224.230");
-        st.put("www.slashdot.org",     "66.35.250.151");
-        st.put("www.espn.com",         "199.181.135.201");
-        st.put("www.weather.com",      "63.111.66.11");
-        st.put("www.yahoo.com",        "216.109.118.65");
+        stringNode.put("www.cs.princeton.edu", "128.112.136.12");
+        stringNode.put("www.cs.princeton.edu", "128.112.136.11");
+        stringNode.put("www.princeton.edu",    "128.112.128.15");
+        stringNode.put("www.simpsons.com",     "209.052.165.60");
+        stringNode.put("www.apple.com",        "17.112.152.32");
+        stringNode.put("www.yale.edu",         "130.132.143.21");
+        stringNode.put("www.amazon.com",       "207.171.182.16");
+        stringNode.put("www.ebay.com",         "66.135.192.87");
+        stringNode.put("www.cnn.com",          "64.236.16.20");
+        stringNode.put("www.google.com",       "216.239.41.99");
+        stringNode.put("www.nytimes.com",      "199.239.136.200");
+        stringNode.put("www.microsoft.com",    "207.126.99.140");
+        stringNode.put("www.dell.com",         "143.166.224.230");
+        stringNode.put("www.slashdot.org",     "66.35.250.151");
+        stringNode.put("www.espn.com",         "199.181.135.201");
+        stringNode.put("www.weather.com",      "63.111.66.11");
+        stringNode.put("www.yahoo.com",        "216.109.118.65");
 
 
-        System.out.println("cs.princeton.edu:  " + st.get("www.cs.princeton.edu"));
-        System.out.println("hardvardsucks.com: " + st.get("www.harvardsucks.com"));
-        System.out.println("simpsons.com:      " + st.get("www.simpsons.com"));
-        System.out.println("apple.com:         " + st.get("www.apple.com"));
-        System.out.println("ebay.com:          " + st.get("www.ebay.com"));
-        System.out.println("dell.com:          " + st.get("www.dell.com"));
+        System.out.println("cs.princeton.edu:  " + stringNode.get("www.cs.princeton.edu"));
+        System.out.println("hardvardsucks.com: " + stringNode.get("www.harvardsucks.com"));
+        System.out.println("simpsons.com:      " + stringNode.get("www.simpsons.com"));
+        System.out.println("apple.com:         " + stringNode.get("www.apple.com"));
+        System.out.println("ebay.com:          " + stringNode.get("www.ebay.com"));
+        System.out.println("dell.com:          " + stringNode.get("www.dell.com"));
         System.out.println();
 
-        System.out.println("size:    " + st.size());
-        System.out.println("height:  " + st.height());
-        System.out.println(st);
+        System.out.println("size:    " + stringNode.size());
+        System.out.println("height:  " + stringNode.height());
+        System.out.println(stringNode);
         System.out.println();
 
         String filepath = "C:\\Users\\Abner\\IdeaProjects\\B-Tree\\src\\main\\java\\words_alpha.txt";
@@ -210,7 +210,7 @@ public class BTree <Key extends Comparable<Key>, Value>{
         fis.close();
 
         String content = sb.toString();
-        st.put(content, "");
+        stringNode.put(content, "");
 
     }
 
