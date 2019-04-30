4f0fa463458a143b6beaf9f2f22918acea7c5904
Abner <axocop@ufm.edu>
2019-04-29 18:44:16 -0600

    Updating hash table

--- Hash functions/HashFunction - Abner/src/main/java/Estructuras/HashTable.java
+++ Hash functions/HashFunction - Abner/src/main/java/Estructuras/HashTable.java
@@ -1,6 +1,8 @@
 package Estructuras;
+import java.util.Random;
 
 public class HashTable {
+    Random rand = new Random();
 
     private final Integer size = 13;
 
@@ -10,7 +12,25 @@ public class HashTable {
 
     private Integer hash (Integer k){
         Integer key = 0;
-        key = Math.abs(k) % size;
+        Integer positive;
+        if(k < 0)
+            positive = k * -1;
+        else positive = k;
+
+        //Integer positive = k * -1;
+        Integer operator2 = positive + size;
+        Integer operator1 = (positive * 5381) + size;
+
+        //key = (Math.abs(k) % size) ;
+        //position = (Math.abs(k) % size) * (rand.nextInt(10));
+        //Integer key = 0;
+
+        if(operator1 > operator2){
+            key = operator1 * operator2;
+        }
+        else {
+            key = operator2 % operator1;
+        }
         return key;
     }
 
@@ -19,7 +39,8 @@ public class HashTable {
     }
 
     public void insertar (Integer k) /*throws Exception*/ {
-        System.out.println(k + " tiene el hash -> " + hash(k));
+        String hex = Integer.toHexString(hash(k));
+        System.out.println(k + " tiene el hash -> " + hex);
 
     }
 
